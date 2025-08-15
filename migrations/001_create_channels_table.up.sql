235d7d9f9959406d117b1e4a148381bad8101def
git <amadeuss.tw@gmail.com>
2025-08-15 15:32:56 +0800

    移除name唯一限制

--- migrations/001_create_channels_table.up.sql
+++ migrations/001_create_channels_table.up.sql
@@ -26,10 +26,6 @@ CREATE INDEX IF NOT EXISTS idx_channels_tags ON channels USING GIN(tags) WHERE d
 CREATE INDEX IF NOT EXISTS idx_channels_created_at ON channels(created_at) WHERE deleted_at IS NULL;
 CREATE INDEX IF NOT EXISTS idx_channels_template_id ON channels(template_id) WHERE deleted_at IS NULL;
 
--- Add constraint for channel_type
-ALTER TABLE channels ADD CONSTRAINT check_channel_type 
-    CHECK (channel_type IN ('email', 'slack', 'sms'));
-
 -- Add constraint for timeout
 ALTER TABLE channels ADD CONSTRAINT check_timeout 
     CHECK (timeout > 0);
@@ -42,6 +38,3 @@ ALTER TABLE channels ADD CONSTRAINT check_retry_attempts
 ALTER TABLE channels ADD CONSTRAINT check_retry_delay 
     CHECK (retry_delay >= 0);
 
--- Add unique constraint on name for non-deleted channels
-CREATE UNIQUE INDEX IF NOT EXISTS idx_channels_name_unique 
-    ON channels(name) WHERE deleted_at IS NULL;
